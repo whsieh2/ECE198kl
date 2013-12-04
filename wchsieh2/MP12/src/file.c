fc8749598bd925a35c7687f52d4662869d394540
wchsieh2 <wchsieh2@c26df54a-155d-4041-bafa-7f83b88924bb>
2013-12-04 04:29:57 +0000

    mp12 submission

--- wchsieh2/MP12/src/file.c
+++ wchsieh2/MP12/src/file.c
@@ -22,7 +22,64 @@ game * create_game(const char * file_name)
     functions.  Make sure to take advantage of allocation functions provided by the game module.
 */
 {
-    game * my_game = NULL;
+    
+    FILE*inputFile = fopen(file_name, "r"); // reads file
+    game * my_game = malloc(sizeof(game)); // allocates memory for the game
+    int rows, cols,i,j;
+    char currentchar;
+    cell* currentcell;
+    fscanf(inputFile, "%d %d %f %f %d", &rows, &cols, &my_game->time_step, &my_game->food_probability, &my_game->growth_per_food);
+    my_game->board = create_board(rows, cols); // calls create_board to create a game
+    char cellarray[rows][cols+1];
+
+    for(i = 0; i < rows; i++)
+    {
+        for(j = 0; j < cols + 1; j++)
+        {
+        	fscanf(inputFile, "%c", &cellarray[i][j]); // takes input and stores it in the cell array
+            currentchar = cellarray[i][j];
+            currentcell = board_cell(my_game->board, i, j); // calls board using board_cell and sets it as the current cell to see what the game should do with that cell
+            if(currentchar != '\n')
+            {
+                switch(currentchar) // uses a switch case to figure out what each individual cell should be based on the current character
+                {
+                    case 'X':
+                        *currentcell = CELL_WALL;
+                        break;
+                    case '-':
+                        *currentcell = CELL_OPEN;
+                        break;
+                        
+                    case 'n':
+                        my_game->human_snake = create_snake(my_game->board, i, j, NORTH, 0);
+                        break;
+                    case 's':
+                        my_game->human_snake = create_snake(my_game->board, i, j, SOUTH, 0);
+                        break;
+                    case 'e':
+                        my_game->human_snake = create_snake(my_game->board, i, j, EAST, 0);
+                        break;
+                    case 'w':
+                        my_game->human_snake = create_snake(my_game->board, i, j, WEST, 0);
+                        break;
+                        
+                    case 'N':
+                        my_game->computer_snake = create_snake(my_game->board, i, j, NORTH, 0);
+                        break;
+                    case 'S':
+                        my_game->computer_snake = create_snake(my_game->board, i, j, SOUTH, 0);
+                        break;
+                    case 'E':
+                        my_game->computer_snake = create_snake(my_game->board, i, j, EAST, 0);
+                        break;
+                    case 'W':
+                        my_game->computer_snake = create_snake(my_game->board, i, j, WEST, 0);
+                        break;
+                }
+            }
+        }
+    }
+    fclose(inputFile); // close the file, the game has been made.
     return my_game; /* return game */
 }
 
@@ -30,4 +87,10 @@ void destroy_game(game * cur_game)
 /*! Deallocate any memory acquired with malloc associated with the given game instance.
     This includes any substructures the game data structure contains.  Make sure to take
     advantage of deallocation functions provided by the game module. */
-{}
+{
+	destroy_snake(cur_game->computer_snake);
+	destroy_board(cur_game->board);
+	destroy_snake(cur_game->human_snake);
+	
+	free(cur_game);
+}
--- wchsieh2/MP12/src/game.c
+++ wchsieh2/MP12/src/game.c
@@ -6,6 +6,17 @@
     Also provides functions for creating, destroying, and accessing game elements.
 */
 
+/*
+INTRO:
+In append snake head I added a new segment by creating new memory. I added a new segment to the head.
+In remove snake tail, I followed the directions given, and removed the last segment of the snake. Also if
+removing of the tail resulted in no segments, I set tail and head to NULL.
+In update snake head, I used the snakes heading to decide what to do with the coordinates. Then I appended the head based on
+if the next cell was open or had food. In update snake tail, I followed the instructions and looked at the growth counter first.
+If it was 0, I'd advance the tail, otherwise if it was positive I would decrement the counter and not move the tail.
+
+*/
+
 #include "game.h"
 
 #include <stdlib.h>
@@ -16,7 +27,8 @@ cell * board_cell(board * cur_board, int row, int col)
     This function should be handy for accessing board cells.
 */
 {
-    cell * cell_ptr = NULL;
+	cell * cell_ptr;
+    cell_ptr =  &cur_board->cells[row*cur_board->cols+col];
     return cell_ptr; /*return cell pointer*/
 }
 
@@ -32,7 +44,12 @@ void randomly_add_food(board * cur_board, float probability)
     library, in order to produce deterministic results that make bugs
     easier to track down.  (i.e. just use random without calling srandom)
 */
-{}
+{
+    long foodCell = random() % (long)(cur_board->rows * cur_board->cols / probability);// determines a random cell based on probability and random number generators.
+    // the division by probability determines the rate at which food spawns
+    if (foodCell < cur_board->rows * cur_board->cols && cur_board->cells[foodCell] == CELL_OPEN)
+    	cur_board->cells[foodCell] = CELL_FOOD;
+}
 
 board * create_board(int rows, int cols)
 /*! Create an instance of a board structure with the given number of rows
@@ -45,13 +62,20 @@ board * create_board(int rows, int cols)
     memory in one call (see "calloc" man page)
 */
 {
-    board * myBoard = NULL;
+    board * myBoard = malloc(sizeof(board));
+    myBoard->cells = calloc(rows*cols,sizeof(cell)); //creates empty dynamic memory for the board
+    myBoard->rows = rows;
+    myBoard->cols=cols;
+
     return myBoard; /*return board */
 }
 
 void destroy_board(board * cur_board)
 /*! Free memory allocated by create_board to create the given board instance. */
-{}
+{
+    free(cur_board->cells);
+    free(cur_board);
+}
 
 snake * create_snake(board * cur_board, int row, int col, direction heading, int initial_growth)
 /*! Create a single-segment snake (See game.h for data structure specification)
@@ -80,19 +104,97 @@ snake * create_snake(board * cur_board, int row, int col, direction heading, int
 
 void destroy_snake(snake * cur_snake)
 /*! Free memory associated with the given snake instance. */
-{}
+{
+
+	snake_segment *delete = cur_snake->head;
+	while(delete !=NULL)
+	{
+		delete = delete->next;
+		free(cur_snake->head); //traverses through the linked list and deletes each node.
+		cur_snake->head = delete;
+	}
+	free(cur_snake);
+}
 
 void append_snake_head(snake * cur_snake, board * cur_board, int row, int col)
-{}
+{	
+	//create a new segment in memory
+	snake_segment *newSegment = malloc(sizeof(snake_segment));
+	//update the new segment's data
+    newSegment->next = NULL;
+    newSegment->row = row;
+    newSegment->col = col;
+    
+	//make the new segment the head.
+    cur_snake->head->next = newSegment;
+    cur_snake->head = newSegment;
+    //establish that the next cell will be a snake.
+    *board_cell(cur_board, row, col) = CELL_SNAKE;
+
+}
 
 void remove_snake_tail(snake * cur_snake, board * cur_board)
-{}
+{
+    snake_segment *deleteSegment = cur_snake->tail;
+    cur_snake->tail = cur_snake->tail->next;
+    //deletes the tail
+    free(deleteSegment);
+    if (cur_snake->length == 1)
+    {
+    
+		cur_snake->head = NULL;
+ 		cur_snake->tail = NULL;
+    }
+    //the cell is now open because it was deleted.
+    cur_board->cells = CELL_OPEN;	
+    cur_snake->length--;
+
+}
 
 void update_snake_head(snake * cur_snake, board * cur_board, int growth_per_food)
-{}
+{
+	if(cur_snake->head == NULL && cur_snake->tail == NULL)
+    {
+        return; // check to see if head/tail are null
+    }
+    int row = cur_snake->tail->row;
+    int col = cur_snake->tail->col; // get the row, col of the linked list tail
+   
+    // see the direction the snake is heading
+	if (cur_snake->heading == NORTH) //if it's north, decrement the row number
+		row--;
+	else if (cur_snake->heading ==  SOUTH) // if it's south, increment the row number
+		row++;
+	else if (cur_snake->heading ==  EAST) // if it's east, increment the col number
+		col++;
+	else if (cur_snake->heading ==  WEST) // if it's west, decrement the col number
+		col--;
+    
+    if(*board_cell(cur_board, row, col) == CELL_OPEN) // if the next cell is open or food, then update the snake head
+    {
+        append_snake_head(cur_snake, cur_board, row, col); // append snake head 
+    }    
+	if(*board_cell(cur_board, row, col) == CELL_FOOD) // if the next cell is food, then increment the food counter and add the growth per food to the growth counter
+	{
+		cur_snake->food++; 
+		cur_snake->growth = cur_snake->growth + growth_per_food;
+		append_snake_head(cur_snake, cur_board, row, col); // append snake head 
+	}
+}
 
 void update_snake_tail(snake * cur_snake, board * cur_board)
-{}
+{
+    
+    if(cur_snake->head == NULL && cur_snake->tail == NULL) // check to see if head or tail is null
+    {
+        return;
+    }
+	else if(cur_snake->growth ==0) //if the growth is 0, I removed the tail
+		remove_snake_tail(cur_snake, cur_board);
+	else //otherwise I decrement the growth.
+		cur_snake->growth--;
+}
+
 
 int next_frame(game * cur_game)
 /*! Modify the state of the given snake game to reflect one game iteration.
